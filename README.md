054cfd777025689034e7ef04c889afa97946d0d3
Cody Bennett <9438817+codybennett@users.noreply.github.com>
2020-10-30 11:43:02 -0500

    Update README and modify deduplication script

--- README.md
+++ README.md
@@ -1,5 +1,66 @@
-# deduplicate-elasticsearch
-A python script to detect duplicate documents in Elasticsearch. Once duplicates have been detected, it is straightforward to call a delete operation to remove duplicates.
+# elasticsearch-deduplication
+_Elasticsearch Deduplication Script_
+---
 
-For a full description on how this script works including an analysis of the memory requirements, see: https://alexmarquardt.com/2018/07/23/deduplicating-documents-in-elasticsearch/
+A simple Python script which assists in removing duplicate documents in Elasticsearch
+
+For a full description on the code this is based on and how it works including an analysis of the memory requirements, see: https://alexmarquardt.com/2018/07/23/deduplicating-documents-in-elasticsearch/
+
+---
+**Requires:**
+
+Access to Elasticsearch 
+
+Python 3.6+
+
+pip
+
+Python Packages
+
+* [elasticsearch](https://github.com/elastic/elasticsearch-py) 
+
+  - _"Official low-level client for Elasticsearch. Its goal is to provide common ground for all Elasticsearch-related code in Python; because of this it tries to be opinion-free and very extendable."_
+
+---
+This is accomplished by:
+
+* Scrolling through documents in specifed index, currently designed for month-based index patterns, filtered by optional Elasticsearch Query
+* Creating a hash of a document based on defined criteria
+* Using the "pop()" method for Python Lists to ensure one copy of document remains and deleting the rest 
+
+The following files are expected to exist in the directory
+from which this module is executed:
+
+* secrets.py  
+    - A collection of Elasticsearch vars and Authentication credentials with expected defintions:
+        - ES_HOST = "URL_WITHOUT_SCHEMA"
+        - ES_USER = "elastic"
+        - ES_PASSWORD = "elastic"
+        - ES_PORT = "9200"
+        - ES_INDEX = "source-YYYY."
+---
+
+Example Usage:
+```
+import hashlib
+from elasticsearch import Elasticsearch, helpers
+import datetime
+
+es = Elasticsearch([{'host':ES_HOST, 'port': ES_PORT, 'use_ssl': True}], http_auth=(ES_USER, ES_PASSWORD))
+dict_of_duplicate_docs = {}
+
+def main():
+    # Define start and end date for deduplication periods
+    # delta is increments to step through
+    #   - smaller increment has a lower memory overhead for the host running the script, with the tradeoff of more queries sent to Elasticsearch; too small an increment results in either no duplicates being found or Elasticsearch being overwhelmed by too many requests
+    #   - larger increment has more memory overhead for host running script but less queries sent to Elasticsearch, one may need to increase request timeout values if using a very large delta
+    start_date = datetime.datetime(2019, 1, 1)
+    end_date = datetime.datetime(2019, 12, 31)
+    delta = datetime.timedelta(days=1)
+
+    while start_date <= end_date:
+        scroll_over_all_docs(start_date)
+        loop_over_hashes_and_remove_duplicates(start_date)
+        start_date += delta
+```
 
--- deduplicate-elaticsearch.py
+++ deduplicate-elaticsearch.py
@@ -1,21 +1,23 @@
-#!/usr/local/bin/python3
-
-# A description and analysis of this code can be found at 
+# A description and analysis of the code this is forked from can be found at
 # https://alexmarquardt.com/2018/07/23/deduplicating-documents-in-elasticsearch/
 
 import hashlib
 from elasticsearch import Elasticsearch, helpers
+import datetime
+import secrets
 
-ES_HOST = 'localhost:9200'
-ES_USER = 'elastic'
-ES_PASSWORD = 'elastic'
+ES_HOST = secrets.ES_HOST
+ES_USER = secrets.ES_USER
+ES_PASSWORD = secrets.ES_PASSWORD
+ES_PORT = secrets.ES_PORT
+ES_INDEX = secrets.ES_INDEX
 
-es = Elasticsearch([ES_HOST], http_auth=(ES_USER, ES_PASSWORD))
+es = Elasticsearch([{'host': ES_HOST, 'port': ES_PORT, 'use_ssl': True}], http_auth=(ES_USER, ES_PASSWORD))
 dict_of_duplicate_docs = {}
 
 # The following line defines the fields that will be
 # used to determine if a document is a duplicate
-keys_to_include_in_hash = ["CAC", "FTSE", "SMI"]
+keys_to_include_in_hash = ["@timestamp","logsource","message"]
 
 
 # Process documents returned by the current search/scroll
@@ -40,32 +42,68 @@ def populate_dict_of_duplicate_docs(hit):
 
 # Loop over all documents in the index, and populate the
 # dict_of_duplicate_docs data structure.
-def scroll_over_all_docs():
-    for hit in helpers.scan(es, index='stocks'):
+def scroll_over_all_docs(tmonth):
+    tdelta = datetime.timedelta(days=1)
+    enddate = (tmonth+tdelta)-datetime.timedelta(microseconds=1)
+    print(tmonth)
+    print(enddate)
+    for hit in helpers.scan(es, index=ES_INDEX+str(tmonth.month).zfill(2), request_timeout=120 ,query={"query": {
+    "bool": {
+      "must": [],
+      "filter": [
+        {
+          "range": {
+            "@timestamp": {
+              "gte": datetime.datetime.strftime(tmonth,"%Y-%m-%dT%H:%M:%S%z"),
+              "lte": datetime.datetime.strftime(enddate,"%Y-%m-%dT%H:%M:%S%z"),
+              "format": "strict_date_hour_minute_second"
+            }
+          }
+        }
+      ],
+      "should": [],
+      "must_not": []
+    }
+  }}):
         populate_dict_of_duplicate_docs(hit)
 
 
-def loop_over_hashes_and_remove_duplicates():
+def loop_over_hashes_and_remove_duplicates(tmonth):
     # Search through the hash of doc values to see if any
     # duplicate hashes have been found
+    topush=[]
     for hashval, array_of_ids in dict_of_duplicate_docs.items():
       if len(array_of_ids) > 1:
-        print("********** Duplicate docs hash=%s **********" % hashval)
-        # Get the documents that have mapped to the current hasval
-        matching_docs = es.mget(index="stocks", doc_type="doc", body={"ids": array_of_ids})
-        for doc in matching_docs['docs']:
-            # In order to remove the possibility of hash collisions,
-            # write code here to check all fields in the docs to
-            # see if they are truly identical - if so, then execute a
-            # DELETE operation on all except one.
-            # In this example, we just print the docs.
-            print("doc=%s\n" % doc)
+        # Pop out a doc from the array of duplicates to ensure at least one copy exists
+        array_of_ids.pop()
+        for doc in array_of_ids:
+            # Add document delete operation to list of queries we will be sending to Elasticsearch via Bulk 
+            topush.append({'_op_type':'delete','_index':ES_INDEX+str(tmonth.month).zfill(2),'_id':doc})
+    # Print how many are being deleted
+    print(len(topush))
+    # Send list of delete queries to Elasticsearch via Bulk
+    helpers.bulk(client=es, actions=topush, stats_only=True, request_timeout=120, raise_on_error=False)
 
-
+       
 
 def main():
-    scroll_over_all_docs()
-    loop_over_hashes_and_remove_duplicates()
+    # Define start and end date for deduplication periods
+    # delta is increments to step through
+    #   - smaller increment has a lower memory overhead for the host running the script, with the tradeoff of more queries sent to Elasticsearch
+    #     too small an increment results in either no duplicates being found or Elasticsearch being overwhelmed by too many requests
+    #   - larger increment has more memory overhead (1 Month is about 4GB) for host running script but less queries sent to Elasticsearch
+    #     one may need to increase request timeout values if using a large increment (days=30, days=60, days=180 etc )
+    # Start and End date of deduplication period, stored as Python Datetime object in format (YYYY, M, D)
+    start_date = datetime.datetime(2019, 2, 3)
+    end_date = datetime.datetime(2019, 9, 30)
+    # How many docs we want to check at a time, one hour equals hour-blocks, one day equals daily etc.
+    delta = datetime.timedelta(days=1)
+
+    # Start looping through docs, searching and deleting in increments of delta 
+    while start_date <= end_date:
+        scroll_over_all_docs(start_date)
+        loop_over_hashes_and_remove_duplicates(start_date)
+        start_date += delta
 
 
 main()
